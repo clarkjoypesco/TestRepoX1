3a672fce868eb3333747e8a4159f516141632d00
Clark Joy Pesco <clarkjoypesco@yahoo.com>
2016-10-21 10:39:35 +0800

    Added Dragon

--- Assets/Scripts/MainMenuScript/MainMenu.cs
+++ Assets/Scripts/MainMenuScript/MainMenu.cs
@@ -6,6 +6,7 @@ public class MainMenu : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		print ("Hello World!!");
+		print ("Hello Dragon!!");
 	}
 	
 	// Update is called once per frame
